2596e8d97371885bf7dae4aa5a59f4ac222f14f6
nesskorin <ness.korin@gmail.com>
2024-05-05 18:00:57 +0100

    updated pages

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-//include('header.php');
-//$var = "Hello World";
-echo "Hello World";
-//print_r($var);
-//include('footer.php');
+include('header.php');
+?>
+<div class="container px-4">
+  <div class="row gx-5">
+    <div class="col">
+<h1>Welcome to Waqq.ly</h1>
+<p>Waqq.ly is the new way to connect dog walkers to dog owners. Our objective is the happy and walked dogs means peace of mind for dog walkers and owners alike.</p>
+<p>Sign up today and let's get tails waqqing!!!</p>
+<a href="userreg.php" class="btn btn-primary btn-lg">Dog Owner Sign Up</a>
+<a href="walkerreg.php" class="btn btn-primary btn-lg">Dog Walker Sign Up</a>
+<div class="col">
+    </div>
+  </div>
+</div>
+<?php
+include('footer.php');
 ?>
